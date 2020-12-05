be160e2dc865f6c132b4a720aeb195aa12e534dd
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-05 03:28:00 -0500

    models for seeding mongo

---
+++ models/workout.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: { type: Date, default: Date.now },
+    exercises:  [
+      {
+        type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+      }
+    ]
+
+  });
+
+const Workout = mongoose.model("Workout",workoutSchema);
+
+module.exports = Workout;
