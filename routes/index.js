646c91000a48bd5e9d2872aa29893838f9dd7567
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-06 04:39:28 -0500

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,11 @@
 const express = require("express");
-// const apiRoute = require("./api-routes");
-// const htmlRoute = require("./html-routes");
-const path = require("path");
+const apiRoute = require("./api-routes");
+const htmlRoute = require("./html-routes");
 
 const router = express.Router();
 
-router.get("/",(req,res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+router.use("/api", apiRoute);
 
-router.get("/exercise",(req,res) => {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
-});
-
-router.get("/stats",(req,res) => {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
-});
+router.use("/", htmlRoute);
 
 module.exports = router;
