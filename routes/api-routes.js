ac610b7dd200666e15696be77755df01cf1b0ef3
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-06 04:42:21 -0500

    updated routes

---
+++ routes/api-routes.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const db = require("../models");
+const router = express.Router();
+
+//get all workouts
+router.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+//get all workouts in range
+router.get("/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+// get workout by id
+router.get("/workouts/:id", (req, res) => {
+  db.Workout.findById(req.params.id)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+//create a workout
+router.post("/workouts", (req, res) => {
+  console.log(req.url);
+  db.Workout.create(res.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+// add an exercise
+router.put("/workouts/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  console.log(req.url);
+  db.Workout.updateOne({ _id: req.params.id }, req.body).then((dbWorkout) => {
+    res.json(dbWorkout);
+  });
+});
+
+//return some JSON
+router.get("/", (req, res) => {
+  res.json({ app: "workout-tracker", url: req.url, version: "0.0.1" });
+});
+
+module.exports = router;
