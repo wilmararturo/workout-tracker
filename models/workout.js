8e0144c3aa601eff669da5dd283344b205a5e7dd
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-06 04:39:55 -0500

    fixed arrary

--- models/workout.js
+++ models/workout.js
@@ -1,22 +1,14 @@
 const mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-  {
-    day: { type: Date, default: Date.now },
-    exercises:  [
-      {
-        type: String,
-        name: String,
-        duration: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number,
-      }
-    ]
+const workoutSchema = new Schema({
+  day: { type: Date, default: Date.now },
+  exercises: {
+    type: Array,
+  },
+});
 
-  });
-
-const Workout = mongoose.model("Workout",workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
